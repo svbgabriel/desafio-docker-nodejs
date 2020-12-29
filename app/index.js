fcccda150db4e45b770b899a9c57186549ab504e
Gabriel Batista <svbgabriel@gmail.com>
2020-12-28 23:00:45 -0300

    Implementa conexão com o banco de dados

--- app/index.js
+++ app/index.js
@@ -12,14 +12,27 @@ const config = {
 const mysql = require('mysql');
 const connection = mysql.createConnection(config);
 
-const create = "";
-const query = "";
+const create = "CREATE TABLE IF NOT EXISTS " +
+    "people (id int AUTO_INCREMENT PRIMARY KEY, name varchar(255) NOT NULL)";
+const insert = "INSERT INTO people (name) VALUES ('Fulano')";
+const query = "SELECT name FROM people";
 
-connection.query(query);
+connection.query(create);
 connection.end();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle</h1>')
+    const connection = mysql.createConnection(config);
+    connection.query(insert);
+    let html = `<h1>Full Cycle Rocks!</h1><br><ul>`;
+    connection.query(query, function (error, results, fields) {
+        if (error) throw error;
+        results.forEach(result => {
+            html += `<li>${result.name}</li>`;
+        });
+        html += '</ul>';
+        res.send(html);
+    });
+    connection.end();
 })
 
 app.listen(port, () => {
